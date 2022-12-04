9093c7d16fcf2d4fbe978fe651d5b44a07f656f7
yanyankangkang <kanghuangnj@gmail.com>
2022-12-04 20:19:05 +0000

    save progress

--- week_2/workspaces/project/week_2.py
+++ week_2/workspaces/project/week_2.py
@@ -5,17 +5,32 @@ from workspaces.resources import mock_s3_resource, redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
 
-@op
-def get_s3_data():
-    pass
+@op(
+    config_schema={"s3_key": String},
+    out = {"stock_list": Out(dagster_type=List[Stock], description="stock data from s3")}, 
+)
+def get_s3_data(context):
+    file_name = context.op_config["s3_key"]
+    return [stock for stock in csv_helper(file_name)]
 
 
-@op
-def process_data():
-    pass
+@op(
+    ins = {"stock_list": In(dagster_type=List[Stock], description="valid stock data")},
+    out = {"aggregation": Out(dagster_type=Aggregation, description="the date with highest stock price")}
+)
+def process_data(context, stock_list):
+    highest = float('-inf')
+    timestamp = None
+    for stock in stock_list:
+        if highest < stock.high:
+            highest = stock.high
+            timestamp = stock.date
+    return Aggregation(date=timestamp, high=highest)
 
-
-@op
+@op(
+    required_resource_keys={"database"},
+    tags={"kind": "redis"},
+)
 def put_redis_data():
     pass
 
